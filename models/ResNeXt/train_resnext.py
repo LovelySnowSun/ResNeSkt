fdb5368dca49ad8251697894e81eb95037fb5a0f
罗雪阳 <65319022+LovelySnowSun@users.noreply.github.com>
2021-03-11 13:42:50 +0800

    Add files via upload

---
+++ models/ResNeXt/train_resnext.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 16:15:37 2021
+
+@author: 78505
+"""
+
+# -*- coding: utf-8 -*-
+import torch,json
+import torch.nn as nn
+from torchvision import transforms, datasets
+import json
+import matplotlib.pyplot as plt
+import os
+import torch.optim as optim
+from resnext import CifarResNeXt
+from thop import profile	
+batch_size = 8
+epoch_all = 200
+lr = 0.0001
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
+    data_transform = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
+    image_path = os.path.join("D:", "imagenet")  # flower data set path
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
+                                         transform=data_transform["train"])
+    train_num = len(train_dataset)
+
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
+    flower_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
+
+
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               )
+
+    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
+                                            transform=data_transform["val"])
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  )
+
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+    
+    net = CifarResNeXt(10,29,10,64,4)
+    net_input = torch.randn(batch_size,3,224,224)
+    flops, params = profile(net, inputs=(net_input,))
+    print("网络计算量GFlops:{},网络参数量#P：{}".format(flops,params))
+    
+    # load pretrain weights
+    # download url: https://download.pytorch.org/models/resnet34-333f7ec4.pth
+    
+    #assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
+    
+    
+    #####读取训练好的参数###############
+    #model_weight_path = "./resNest50.pth"
+    #missing_keys, unexpected_keys = net.load_state_dict(torch.load(model_weight_path), strict=False)
+    #for param in net.parameters():
+    #    param.requires_grad = False
+    #####读取训练好的参数###############
+    # change fc layer structure
+    in_channel = net.classifier.in_features
+    net.classifier = nn.Linear(in_channel, 10)
+    net.to(device)
+
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    best_acc = 0.0
+    save_path = './resNext50.pth'
+    list_loss = list()
+    list_acc = list()
+    for epoch in range(epoch_all):
+        # train
+        net.train()
+        running_loss = 0.0
+        for step, data in enumerate(train_loader, start=0):
+            images, labels = data
+            optimizer.zero_grad()
+            logits = net(images.to(device))
+            loss = loss_function(logits, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            # print train process
+            rate = (step+1)/len(train_loader)
+            a = "*" * int(rate * 50)
+            b = "." * int((1 - rate) * 50)
+            print("\rtrain loss: {:^3.0f}%[{}->{}]{:.4f}".format(int(rate*100), a, b, loss), end="")
+        print()
+
+        # validate
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            for val_data in validate_loader:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))  # eval model only have last output layer
+                # loss = loss_function(outputs, test_labels)
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += (predict_y == val_labels.to(device)).sum().item()
+            val_accurate = acc / val_num
+            if val_accurate > best_acc:
+                best_acc = val_accurate
+                torch.save(net.state_dict(), save_path)
+            print('[epoch %d] train_loss: %.3f  test_accuracy: %.3f' %
+                  (epoch + 1, running_loss / step, val_accurate))
+            list_loss.append(running_loss / step)
+            list_acc.append(val_accurate)
+    #保存loss and acc
+    file_loss_path = "./ResNext_loss.txt"
+    with open(file_loss_path,"w") as file_object:
+        json.dump(list_loss,file_object)
+
+    file_acc_path = "./ResNext_acc.txt"
+    with open(file_acc_path,"w") as file_object:
+        json.dump(list_acc,file_object)
+    print('Finished Training')
+    
+
+if __name__ == '__main__':
+    main()
